ef723a09b59817a4e55189a754c60fbd712d85b8
Brian Foody <brianfoody@gmail.com>
2020-01-19 20:40:50 +0800

    Final form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,120 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import config from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          {config.API_URL}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      company: "",
+      message: "",
+      submitted: false,
+      loading: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.name || !this.state.company || !this.state.message) {
+      window.alert("Yo! We need all your details.");
+    }
+
+    this.submit();
+  }
+
+  submit = async () => {
+    try {
+      this.setState({
+        loading: true
+      });
+      await fetch(config.API_URL + "contact", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          name: this.state.name,
+          company: this.state.company,
+          message: this.state.message
+        })
+      });
+
+      this.setState({
+        name: "",
+        company: "",
+        message: "",
+        submitted: true
+      });
+    } catch (err) {
+      window.alert(
+        "Well this is embarrassing. Our 'Server' is down. Send us an email 😂"
+      );
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Contact Us</h1>
+        </header>
+
+        <section>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+
+            <label htmlFor="company">Company</label>
+            <input
+              type="text"
+              name="company"
+              id="company"
+              value={this.state.company}
+              onChange={this.handleChange}
+            />
+
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              value={this.state.message}
+              onChange={this.handleChange}
+            />
+
+            {this.state.loading ? (
+              <p class="submitLoader"> Submitting....</p>
+            ) : (
+              <input type="submit" value="Submit" />
+            )}
+
+            {this.state.submitted && (
+              <p class="successMessage">
+                Thanks for contacting us, we'll be in touch shortly.
+              </p>
+            )}
+          </form>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config/aws.config.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  "https://uf5ex8ftbl.execute-api.ap-southeast-2.amazonaws.com/prod/";
